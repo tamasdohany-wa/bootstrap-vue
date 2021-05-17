dc72bbb70380c61e309107554ff3e7621ecc7779
Tamas Dohany <tamas.dohany@webapix.hu>
2021-05-18 00:51:30 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <b-icon icon="exclamation-circle-fill" variant="success"></b-icon>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Vue from 'vue'
+import { BootstrapVueIcons } from 'bootstrap-vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BootstrapVueIcons,
   }
 }
+Vue.use(BootstrapVueIcons)
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+svg {
+  fill: red;
 }
 </style>
