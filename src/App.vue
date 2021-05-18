eea7c03f82e3640f81c9147a33591e5e01b5ca46
Tamas Dohany <tamas.dohany@webapix.hu>
2021-05-18 09:18:53 +0200

    align

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <section>
-    <b-icon icon="exclamation-circle-fill" variant="success"></b-icon>
-    <b-icon icon="exclamation-circle-fill" variant="warning"></b-icon>
-    <b-icon icon="exclamation-circle-fill" variant="danger"></b-icon>
-    <b-icon icon="exclamation-circle-fill" variant="info"></b-icon>
-    <b-icon icon="exclamation-circle-fill" variant="primary"></b-icon>
-    <b-icon icon="exclamation-circle-fill" variant="secondary"></b-icon>
-    <b-icon icon="exclamation-circle-fill" variant="dark"></b-icon>
+    <b-icon icon="exclamation-circle-fill" variant="success" title="success"></b-icon>
+    <b-icon icon="exclamation-circle-fill" variant="warning" title="warning"></b-icon>
+    <b-icon icon="exclamation-circle-fill" variant="danger" title="danger"></b-icon>
+    <b-icon icon="exclamation-circle-fill" variant="info" title="info"></b-icon>
+    <b-icon icon="exclamation-circle-fill" variant="primary" title="primary"></b-icon>
+    <b-icon icon="exclamation-circle-fill" variant="secondary" title="secondary"></b-icon>
+    <b-icon icon="exclamation-circle-fill" variant="dark" title="dark"></b-icon>
   </section>
 </template>
 
@@ -24,6 +24,14 @@ export default {
 Vue.use(BootstrapVueIcons)
 </script>
 
-<style>
-/**/
+<style scoped>
+section {
+  min-height: 100vh;
+  display: grid;
+  place-content: center;
+  text-align: center;
+}
+section svg {
+  margin: .25rem 0;
+}
 </style>
