7e7063e007d959052f20492d3fe9075a0642adbb
Tamas Dohany <tamas.dohany@webapix.hu>
2021-05-18 08:57:51 +0200

    twbs css

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,29 @@
 <template>
-  <b-icon icon="exclamation-circle-fill" variant="success"></b-icon>
+  <section>
+    <b-icon icon="exclamation-circle-fill" variant="success"></b-icon>
+    <b-icon icon="exclamation-circle-fill" variant="warning"></b-icon>
+    <b-icon icon="exclamation-circle-fill" variant="danger"></b-icon>
+    <b-icon icon="exclamation-circle-fill" variant="info"></b-icon>
+    <b-icon icon="exclamation-circle-fill" variant="primary"></b-icon>
+    <b-icon icon="exclamation-circle-fill" variant="secondary"></b-icon>
+    <b-icon icon="exclamation-circle-fill" variant="dark"></b-icon>
+  </section>
 </template>
 
 <script>
 import Vue from 'vue'
 import { BootstrapVueIcons } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+
 export default {
   name: 'App',
   components: {
-    BootstrapVueIcons,
+    BootstrapVueIcons
   }
 }
 Vue.use(BootstrapVueIcons)
 </script>
 
 <style>
-svg {
-  fill: red;
-}
+/**/
 </style>
